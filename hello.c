3e2515484fcd8ef57aa80ac5bcada716f312a760
Tim Bird <tim.bird@am.sony.com>
2016-08-11 16:09:04 -0700

    Add randomized success or failure

--- hello.c
+++ hello.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+int randint(int bottom, int top)
+{
+	int range;
+
+	range = (top - bottom) + 1;
+	return(rand() % range) + bottom;
+}
 
 int main(int argc, char **argv)
 {
 	printf("hello-test says Hello!\n");
-	printf("status: OK!\n");
+
+	srand(time(NULL));
+	if (randint(1,10)<=5) {
+		printf("status: OK!\n");
+		return 0;
+	} else {
+		printf("status: Failure!\n");
+		return -1;
+	}
 }
