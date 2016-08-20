cd104d53b58b291ca5641ec8c69ff868ad4343bb
Tim Bird <tim.bird@am.sony.com>
2016-08-20 16:49:08 -0700

    Add support for -f and for making a distribution tarfile
    -f is used to force the test to fail
    -r is used to randomly fail the test
    -h shows command usage (so does --help)

--- Makefile
+++ Makefile
@@ -1,10 +1,20 @@
 all: hello
 
+VERSION=1.0
+TARFILE=hello-test-${VERSION}.tgz
+
 hello: hello.c
-	gcc $< -o $@
+	${CC} $< -o $@
 
 clean:
-	rm hello
+	rm -f hello
 
 test:
-	@./hello
+	@./hello -r
+
+distclean: clean
+	rm -f ../${TARFILE} ../hello-test-${VERSION}
+
+dist: clean
+	ln -sf hello-test ../hello-test-${VERSION}
+	tar -czvf ../${TARFILE} -C .. hello-test-${VERSION}/
--- hello.c
+++ hello.c
@@ -1,6 +1,25 @@
+/***************************************
+ * hello.c - print "Hello World" and simulate test execution
+ * This is for example, and for testing test systems
+ *
+ * Copyright 2016 Sony Corporation
+ * Author: Tim Bird <tim.bird@am.sony.com
+ * License: MIT
+ ***************************************/
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <sys/time.h>
+
+void usage(void)
+{
+	printf("Usage: hello [-h|-f|-r]\n\n");
+	printf("Print \"Hello world\", and show a testing result.\n");
+	printf("Options:\n");
+	printf(" -h   Show this test usage\n");
+	printf(" -f   Force the test to fail\n");
+	printf(" -r   Randomaly have the test succeed or fail\n");
+	printf("      The test should succeed (and fail) about 50%% of the time\n"); }
 
 int randint(int bottom, int top)
 {
@@ -12,14 +31,38 @@ int randint(int bottom, int top)
 
 int main(int argc, char **argv)
 {
-	printf("hello-test says Hello!\n");
+	int failtest;
 
-	srand(time(NULL));
-	if (randint(1,10)<=5) {
-		printf("status: OK!\n");
-		return 0;
-	} else {
-		printf("status: Failure!\n");
+	printf("Hello World!\n\n");
+
+	printf("This is a test program, to demonstrate test success and failure\n");
+
+	// check arguments, and simulate test execution
+	failtest = 0;
+	if (argc >= 2) {
+		if (strcmp(argv[1], "-h")==0 ||
+		    strcmp(argv[1], "--help")==0 ) {
+			usage();
+			exit(0);
+		}
+		if (strcmp(argv[1], "-f")==0) {
+			failtest = 1;
+		}
+		if (strcmp(argv[1], "-r")==0) {
+			srand(time(NULL));
+			if (randint(1,10)<=5) {
+				failtest = 1;
+			}
+		}
+	}
+	
+	// show "test" results
+	printf("Test result: ");
+	if (failtest) {
+		printf("FAIL\n");
 		return -1;
+	} else {
+		printf("SUCCESS\n");
+		return 0;
 	}
 }
